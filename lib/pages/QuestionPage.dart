753acd62b7a9aba0a2d6898a2ef96da2125fbe2d
jhhan <omaro753762@gmail.com>
2023-07-21 16:19:13 +0900

    문제화면 추가 ㄴ 버전, 정답모드, 랜덤모드, 시험모드 변수 확인

--- lib/pages/QuestionPage.dart
+++ lib/pages/QuestionPage.dart
@@ -5,21 +5,23 @@ import 'package:careexam/class/Init.dart';
 import 'package:lottie/lottie.dart';
 import '../model/QuestionS.dart';
 
-class MainPage extends StatefulWidget {
-  const MainPage({required Key key}) : super(key: key);
+class QuestionPage extends StatefulWidget {
+  final String version;
+  final bool isAnswerMode, isRandomMode, isExamMode;
+  const QuestionPage({required Key key, required this.version, required this.isAnswerMode, required this.isRandomMode, required this.isExamMode}) : super(key: key);
   @override
-  _MainPageState createState() => _MainPageState();
+  _QuestionPageState createState() => _QuestionPageState();
 }
 
-class _MainPageState extends State<MainPage> {
+class _QuestionPageState extends State<QuestionPage> {
 
   late Future<List<QuestionS>> questionList;
 
   @override
   void initState() {
 
-    //Logger logger = Logger();
-    //logger.d(globals.mQuestionList.length);
+    Logger logger = Logger();
+    logger.d("version : ${widget.version}, isAnswerMode : ${widget.isAnswerMode}, isRandomMode : ${widget.isRandomMode}, isExamMode : ${widget.isExamMode}");
     questionList = Init.instance.fetchPost();
     setQuestionList();
     super.initState();
