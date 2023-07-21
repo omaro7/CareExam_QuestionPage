513cd5bf5666be0302670de50b7833e148dd4d16
jhhan <omaro753762@gmail.com>
2023-07-21 10:59:00 +0900

    버젼리스트 항목 수정(화면 렌더링성공) ㄴ 글자 수정 ㄴ 위치조정 및 scrollview 위치 조정

--- lib/pages/VersionListPage.dart
+++ lib/pages/VersionListPage.dart
@@ -14,6 +14,8 @@ class VersionListPage extends StatefulWidget {
 class _VersionListState extends State<VersionListPage> {
 
   late Future<List<QuestionS>> mVersionList;
+  bool _isAnswerEnabled = false;
+  bool _isRandomEnabled = false;
   bool _isExamEnabled = false;
 
   @override
@@ -47,7 +49,31 @@ class _VersionListState extends State<VersionListPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xffedecec),
+      backgroundColor: Color(0xff000000),
+      appBar: AppBar(
+        elevation: 0,
+        centerTitle: true,
+        automaticallyImplyLeading: false,
+        backgroundColor: Colors.transparent,  //투명
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.zero,
+        ),
+        title: Text(
+          "야! 너두 자격증 딸 수 있어!",
+          style: TextStyle(
+            fontWeight: FontWeight.w700,
+            fontStyle: FontStyle.normal,
+            fontSize: 24,
+            color: Colors.white,
+          ),
+        ),
+        leading: Icon(
+          Icons.arrow_back,
+          color: Colors.white,
+          size: 24,
+        ),
+      ),
+      extendBodyBehindAppBar: true,   //body 위에 appbar
       body: Stack(
         alignment: Alignment.topLeft,
         children: [
@@ -63,196 +89,202 @@ class _VersionListState extends State<VersionListPage> {
           Container(
             alignment: Alignment.topLeft,
             margin: EdgeInsets.all(0),
-            padding: EdgeInsets.fromLTRB(16, 30, 16, 16),
+            padding: EdgeInsets.fromLTRB(16, 90, 16, 16),
             width: MediaQuery.of(context).size.width,
             height: MediaQuery.of(context).size.height * 0.4,
-            decoration: BoxDecoration(
-              color: Color(0xff3a57e8),
-              shape: BoxShape.rectangle,
-              borderRadius: BorderRadius.zero,
-              border: Border.all(color: Color(0x4d9e9e9e), width: 1),
-            ),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.start,
               crossAxisAlignment: CrossAxisAlignment.center,
               mainAxisSize: MainAxisSize.max,
               children: [
-                Lottie.asset(
-                  "assets/lottie/walking_coffee_time.json",
-                  height: 60,
-                  width: 60,
-                  fit: BoxFit.cover,
-                  repeat: true,
-                  animate: true,
+                Container(
+                alignment: Alignment.center,
+                margin: EdgeInsets.fromLTRB(0, 6, 16, 0),
+                padding: EdgeInsets.all(0),
+                child:
+                  Lottie.asset(
+                    "assets/lottie/wheelchair.json",
+                    height: 80,
+                    width: 80,
+                    fit: BoxFit.cover,
+                    repeat: true,
+                    animate: true,
+                  ),
                 ),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisSize: MainAxisSize.max,
-                  children: [
-                    Align(
-                      alignment: Alignment.center,
-                      child: Text(
-                        "요양보호사 자격증 취득하기",
-                        textAlign: TextAlign.center,
-                        overflow: TextOverflow.clip,
-                        style: TextStyle(
-                          fontWeight: FontWeight.w700,
-                          fontStyle: FontStyle.normal,
-                          fontSize: 24,
-                          color: Color(0xffffffff),
+                Container(
+                  alignment: Alignment.topLeft,
+                  margin: EdgeInsets.fromLTRB(0, 6, 0, 0),
+                  padding: EdgeInsets.all(0),
+                  child:
+                    const Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        Align(
+                          alignment: Alignment.center,
+                          child: Text(
+                            "요양보호사 자격증 취득하기",
+                            textAlign: TextAlign.center,
+                            overflow: TextOverflow.clip,
+                            style: TextStyle(
+                              fontWeight: FontWeight.w700,
+                              fontStyle: FontStyle.normal,
+                              fontSize: 20,
+                              color: Color(0xffffffff),
+                            ),
+                          ),
                         ),
-                      ),
+                        Text(
+                          "누구나 쉽게 요양보호사 자격증을 취득할 수 있다",
+                          textAlign: TextAlign.start,
+                          overflow: TextOverflow.clip,
+                          style: TextStyle(
+                            fontWeight: FontWeight.w400,
+                            fontStyle: FontStyle.normal,
+                            fontSize: 14,
+                            color: Color(0xffffffff),
+                          ),
+                        ),
+                      ],
                     ),
-                    Text(
-                      "누구나 쉽게 요양보호사 자격증을 취득할 수 있다",
-                      textAlign: TextAlign.start,
-                      overflow: TextOverflow.clip,
-                      style: TextStyle(
-                        fontWeight: FontWeight.w400,
-                        fontStyle: FontStyle.normal,
-                        fontSize: 14,
-                        color: Color(0xffffffff),
-                      ),
-                    ),
-                  ],
                 ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisSize: MainAxisSize.max,
-                  children: [
-                    Text(
-                      "정답노출모드",
-                      textAlign: TextAlign.start,
-                      overflow: TextOverflow.clip,
-                      style: TextStyle(
-                        fontWeight: FontWeight.w400,
-                        fontStyle: FontStyle.normal,
-                        fontSize: 14,
-                        color: Color(0xffffffff),
-                      ),
+                Container(
+                  alignment: Alignment.center,
+                  margin: EdgeInsets.fromLTRB(0, 6, 0, 6),
+                  padding: EdgeInsets.all(0),
+                    child:
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        const Text(
+                          "정답노출모드",
+                          textAlign: TextAlign.start,
+                          overflow: TextOverflow.clip,
+                          style: TextStyle(
+                            fontWeight: FontWeight.w400,
+                            fontStyle: FontStyle.normal,
+                            fontSize: 14,
+                            color: Color(0xffffffff),
+                          ),
+                        ),
+                        Switch(
+                          value: _isAnswerEnabled,
+                          onChanged: (value) {
+                            setState((){
+                              _isAnswerEnabled = value;
+                            });
+                          },
+                          activeColor: Color(0xff82e83a),
+                          activeTrackColor: Color(0xffffffff),
+                          inactiveThumbColor: Color(0xff9e9e9e),
+                          inactiveTrackColor: Color(0xffe0e0e0),
+                        ),
+                        const Text(
+                          "랜덤항목",
+                          textAlign: TextAlign.start,
+                          overflow: TextOverflow.clip,
+                          style: TextStyle(
+                            fontWeight: FontWeight.w400,
+                            fontStyle: FontStyle.normal,
+                            fontSize: 14,
+                            color: Color(0xffffffff),
+                          ),
+                        ),
+                        Switch(
+                          value: _isRandomEnabled,
+                          onChanged: (value) {
+                            setState((){
+                              _isRandomEnabled = value;
+                            });
+                          },
+                          activeColor: Color(0xff82e83a),
+                          activeTrackColor: Color(0xffffffff),
+                          inactiveThumbColor: Color(0xff9e9e9e),
+                          inactiveTrackColor: Color(0xffe0e0e0),
+                        ),
+                        const Text(
+                          "시험모드",
+                          textAlign: TextAlign.start,
+                          overflow: TextOverflow.clip,
+                          style: TextStyle(
+                            fontWeight: FontWeight.w400,
+                            fontStyle: FontStyle.normal,
+                            fontSize: 14,
+                            color: Color(0xffffffff),
+                          ),
+                        ),
+                        Switch(
+                            value: _isExamEnabled,
+                            onChanged: (value) {
+                              setState((){
+                                _isExamEnabled = value;
+                              });
+                            },
+                            activeColor: Color(0xff82e83a),
+                            activeTrackColor: Color(0xffffffff),
+                            inactiveThumbColor: Color(0xff9e9e9e),
+                            inactiveTrackColor: Color(0xffe0e0e0),
+                          ),
+                      ],
                     ),
-                    Container(
-                      width: 100,
-                      height: 20,
-                      child:SwitchListTile(
-                        title: const Text('#1'),
-                        value: _isExamEnabled,
-                        onChanged: (value) {
-                          setState((){
-                            _isExamEnabled = value;
-                          });
-                        },
-                        activeColor: Color(0xff82e83a),
-                        activeTrackColor: Color(0xffffffff),
-                        inactiveThumbColor: Color(0xff9e9e9e),
-                        inactiveTrackColor: Color(0xffe0e0e0),
-                      ),
-                    ),
-                    Text(
-                      "랜덤항목",
-                      textAlign: TextAlign.start,
-                      overflow: TextOverflow.clip,
-                      style: TextStyle(
-                        fontWeight: FontWeight.w400,
-                        fontStyle: FontStyle.normal,
-                        fontSize: 14,
-                        color: Color(0xffffffff),
-                      ),
-                    ),
-                    Container(
-                      width: 100,
-                      height: 20,
-                      child:SwitchListTile(
-                        title: const Text('#2'),
-                        value: _isExamEnabled,
-                        onChanged: (value) {
-                          setState((){
-                            _isExamEnabled = value;
-                          });
-                        },
-                        activeColor: Color(0xff82e83a),
-                        activeTrackColor: Color(0xffffffff),
-                        inactiveThumbColor: Color(0xff9e9e9e),
-                        inactiveTrackColor: Color(0xffe0e0e0),
-                      ),
-                    ),
-                    Text(
-                      "시험모정",
-                      textAlign: TextAlign.start,
-                      overflow: TextOverflow.clip,
-                      style: TextStyle(
-                        fontWeight: FontWeight.w400,
-                        fontStyle: FontStyle.normal,
-                        fontSize: 14,
-                        color: Color(0xffffffff),
-                      ),
-                    ),
-                    Container(
-                      width: 100,
-                      height: 20,
-                      child:SwitchListTile(
-                        title: const Text('#3'),
-                        value: _isExamEnabled,
-                        onChanged: (value) {
-                          setState((){
-                            _isExamEnabled = value;
-                          });
-                        },
-                        activeColor: Color(0xff82e83a),
-                        activeTrackColor: Color(0xffffffff),
-                        inactiveThumbColor: Color(0xff9e9e9e),
-                        inactiveTrackColor: Color(0xffe0e0e0),
-                      ),
-                    ),
-                  ],
                 ),
               ],
             ),
           ),
           Container(
             alignment: Alignment.topLeft,
-            margin: EdgeInsets.fromLTRB(16, 200, 16, 16),
+            margin: EdgeInsets.fromLTRB(16, 300, 16, 16),
             padding: EdgeInsets.all(0),
             width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height * 0.7000000000000001,
+            height: MediaQuery.of(context).size.height * 0.6000000000000001,
             decoration: BoxDecoration(
-              color: Color(0xffffffff),
+              color: Colors.white,
               shape: BoxShape.rectangle,
               borderRadius: BorderRadius.circular(12.0),
             ),
-            child: Padding(
-              padding: EdgeInsets.symmetric(vertical: 0, horizontal: 16),
+            child:Container(
+              alignment: Alignment.topLeft,
               child: SingleChildScrollView(
-                child: FutureBuilder<List<QuestionS>>(
-                  future: getVersionList(),
-                  builder: (context, snapshot) {
-                    if (snapshot.hasData) {
-                      print(snapshot.data?.length); // question
-                      return ListView.builder(
-                        shrinkWrap: true,         // 미지정시, size 에러 발생함.
-                        itemCount: snapshot.data?.length,
-                        itemBuilder: (context, index){
-                          QuestionS questionS = snapshot.data![index];
-                          return Container(
-                              //width: 120,
-                              //height: 40,
-                              child:Card(
-                                child: ListTile(
-                                  title: Text(questionS.res_be_version.toString()),
-                                ),
-                              ),
-                          );
+                child:Column(
+                  children:[
+                    Container(
+                      //alignment: Alignment.topLeft,
+                      padding: EdgeInsets.all(0),
+                      height: MediaQuery.of(context).size.height * 0.5900000000000001,
+                      child:FutureBuilder<List<QuestionS>>(
+                        future: getVersionList(),
+                        builder: (context, snapshot) {
+                          if (snapshot.hasData) {
+                            print(snapshot.data?.length); // question
+                            return ListView.builder(
+                              shrinkWrap: true,         // 미지정시, size 에러 발생함.
+                              itemCount: snapshot.data?.length,
+                              itemBuilder: (context, index){
+                                QuestionS questionS = snapshot.data![index];
+                                return Container(
+                                    //width: 120,
+                                    //height: 40,
+                                    alignment: Alignment.topLeft,
+                                    child:Card(
+                                      child: ListTile(
+                                        title: Text(questionS.res_be_version.toString()),
+                                      ),
+                                    ),
+                                );
+                              },
+                            );
+                          } else if (snapshot.hasError) {
+                            return Text("${snapshot.error}");
+                          }
+                          return CircularProgressIndicator();
                         },
-                      );
-                    } else if (snapshot.hasError) {
-                      return Text("${snapshot.error}");
-                    }
-                    return CircularProgressIndicator();
-                  },
+                      ),
+                    ),
+                  ],
                 ),
-
                 /*
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.start,
